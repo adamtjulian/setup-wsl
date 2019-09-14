495f18a43fe99d48e3fd2495075e69681f612eb1
Adam Julian <atjulian84@gmail.com>
2019-09-14 15:38:37 -0500

    Added vim config in ansible

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-echo "\n\ninstall vim"
-sudo apt-get install -y vim
-echo "\n\ninstall tmux"
-sudo apt-get install -y tmux
 echo "\n\ninstall Ansible"
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+echo "Run Ansible Playbooks"
 ansible-playbook ansible/setup-git.yml
+ansible-playbook ansible/setup-tmux.yml
+ansible-playbook ansible/setup-vim.yml
+ansible-playbook ansible/setup-python.yml
