23e6714649326efe88d13f5bc4783be79ceeedd4
Adam Julian <atjulian84@gmail.com>
2019-09-25 22:32:54 -0500

    Restructure playbook to segrate tasks

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 echo "Run Ansible Playbooks"
+
+echo "Put Ansible Vault Key In Place"
+mkdir /home/atjulian/wsl/
+cp /mnt/c/users/Adam/Repositories/wsl/ansible-key /home/atjulian/wsl/ansible-key
+sudo chmod 0644 /home/atjulian/wsl/ansible-key
+
 ansible-playbook ansible/setup-git.yml
 ansible-playbook ansible/setup-tmux.yml
 ansible-playbook ansible/setup-vim.yml
