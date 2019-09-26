f73115d86e0dd1f0f3e9fc592a7801b20a2eb202
Adam Julian <atjulian84@gmail.com>
2019-09-26 07:58:19 -0500

    Restructure the playbook, changed bash script to reflect new command

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-sudo apt-get update
 echo "\n\ninstall Ansible"
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
-echo "Run Ansible Playbooks"
+sudo apt-get install ansible -y
 
 echo "Put Ansible Vault Key In Place"
 mkdir /home/atjulian/wsl/
 cp /mnt/c/users/Adam/Repositories/wsl/ansible-key /home/atjulian/wsl/ansible-key
 sudo chmod 0644 /home/atjulian/wsl/ansible-key
-
-ansible-playbook ansible/setup-git.yml
-ansible-playbook ansible/setup-tmux.yml
-ansible-playbook ansible/setup-vim.yml
-ansible-playbook ansible/setup-python.yml
+echo "Run Ansible Playbooks"
+sudo ansible-playbook setup.yml --vault-password-file=/home/atjulian/wsl/ansible-key
